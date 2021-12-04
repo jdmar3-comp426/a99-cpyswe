7b94884536da705701265b129d5462a0a97ce554
TaixiL <taixi@email.unc.edu>
2021-12-04 00:45:44 -0500

    comment

--- form.js
+++ form.js
@@ -3,32 +3,37 @@ window.addEventListener("load", function () {
     var isLogIn;
     var current = null;
 
-    function hideElements(){
+    function hideElements(){        // Hide elements that require log in (new game, delte, modify)
+
         document.getElementById("begin").style.display="None";
         document.getElementById("dataPlace").style.display="None";
         document.getElementById("updateForm").style.display="None";
         document.getElementById("deleteAcc").style.display="None";
+
     }
-    function showElements(){
+    function showElements(){        // Show those elements after log in
+
         document.getElementById("begin").style.display="block";
         document.getElementById("dataPlace").style.display="block";
         document.getElementById("updateForm").style.display="block";
         document.getElementById("deleteAcc").style.display="block";
+
     }
+
     hideElements();
 
-    function hideLogin() {
+    function hideLogin() {          // Hide login form after log in
         document.getElementById("logUser").style.display = "None";
         document.getElementById("logPass").style.display = "None";
     }
     
-    function showLogin() {
+    function showLogin() {          // Show login for after log out
         document.getElementById("logUser").style.display = "block";
         document.getElementById("logPass").style.display = "block";
     }
     
     
-    function sendData() {
+    function sendData() {           // Send the form for registration
         const XHR = new XMLHttpRequest();
               FD = new URLSearchParams(new FormData( form ));
         
@@ -45,21 +50,22 @@ window.addEventListener("load", function () {
         XHR.open("POST", "http://localhost:5000/app/new/user");
         XHR.send( FD );
     }
-    const form = document.getElementById("myForm");
+
+    const form = document.getElementById("myForm");     // When submit, call sendData()
     form.addEventListener("submit", function(event){
         event.preventDefault();
         sendData();
     });
 
-    function getUserdata( form ) {
+    function checkLog( form ) {      // Send the form for log in
         const sendRequest = new XMLHttpRequest();
         const userInfo = new URLSearchParams(new FormData( form ));
 
 
         sendRequest.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
+            if (this.readyState == 4 && this.status == 200) {       // If the credential is correct, perform changes
                 current = JSON.parse(sendRequest.response);
-                if(isLogIn){
+                if(isLogIn){                                        // If already logged in, change elements to logged out state
                     
                     current = null;
                     isLogIn = false;
@@ -67,7 +73,8 @@ window.addEventListener("load", function () {
 
                     hideElements()
                     alert("Logged Out");
-                } else {
+
+                } else {                                            // If not logged in, change to logged in
 
                     document.getElementById("login").value = "Logout";
                     isLogIn = true;
@@ -88,10 +95,11 @@ window.addEventListener("load", function () {
 
     
     
-    const selfUser = document.getElementById("loginForm");
+    const selfUser = document.getElementById("loginForm");          // If click submit in the log in form, call function checkLog()
     selfUser.addEventListener("submit", function(event){
         event.preventDefault();
         if(isLogIn){
+
             alert("Logged Out");
             hideElements()
             current = null;
@@ -99,14 +107,13 @@ window.addEventListener("load", function () {
             document.getElementById("login").value = "Login";
             this.reset();
             showLogin();
-            document.getElementById("profileData").innerHTML = ``
 
         } else {
-            getUserdata(this);
+            checkLog(this);
         }
     });
 
-    function updateInfo( form ) {
+    function updateInfo( form ) {                                   // Send form for updating information
         const XHR = new XMLHttpRequest();
         var FD = new URLSearchParams(new FormData( form ));
 
@@ -122,14 +129,14 @@ window.addEventListener("load", function () {
 
     const newData = document.getElementById("updateinfo");
 
-    newData.addEventListener("submit", function(event){
+    newData.addEventListener("submit", function(event){             // If click submit, call updateInfo()
 
         event.preventDefault();
         updateInfo(this);
 
     });
 
-    function deleteUser(form) {
+    function deleteUser(form) {                                     // Send form for deleting account
         const XHR = new XMLHttpRequest();
         var FD = new URLSearchParams(new FormData( form ));
         FD.append('user', current.user);
@@ -143,8 +150,8 @@ window.addEventListener("load", function () {
 
     }
 
-    const deleted = document.getElementById("deleteAcc");
-    deleted.addEventListener("submit", function(event) {
+    const deleted = document.getElementById("deleteAcc");           // If click delete, call deleteUser()
+    deleted.addEventListener("submit", function(event) {    
         event.preventDefault();
         deleteUser(this);
     });
@@ -158,7 +165,7 @@ window.addEventListener("load", function () {
 
     });
 
-    function recordHigh (form) {
+    function recordHigh (form) {                                    // Send the form to record the highest score (This does not seem to work)
         alter("GGGGGG");
         const XHR = new XMLHttpRequest();
         var FD = new URLSearchParams(new FormData( form ));
@@ -181,7 +188,7 @@ window.addEventListener("load", function () {
 
     }
 
-    const highest = document.getElementById("scoreForm");
+    const highest = document.getElementById("scoreForm");           
     highest.addEventListener("submit", function(event) {
         event.preventDefault();
         recordHigh(this);
